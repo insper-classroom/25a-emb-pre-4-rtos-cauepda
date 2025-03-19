1296c2d79b97be18a22f01755b921ccc364b17ce
cauepda <cauepda@al.insper.edu.br>
2025-03-19 18:04:16 -0300

    update

--- exe4/main.c
+++ exe4/main.c
@@ -14,10 +14,16 @@ const int LED_PIN_G = 6;
 
 QueueHandle_t xQueueButId;
 SemaphoreHandle_t xSemaphore_r;
+QueueHandle_t yQueueButId;
+SemaphoreHandle_t xSemaphore_g;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_r, 0);
+    if (events == 0x4) { // borda de descida
+        if (gpio == BTN_PIN_R) {
+            xSemaphoreGiveFromISR(xSemaphore_r, NULL);
+        } else if (gpio == BTN_PIN_G) {
+            xSemaphoreGiveFromISR(xSemaphore_g, NULL);
+        }
     }
 }
 
@@ -36,6 +42,25 @@ void led_1_task(void *p) {
             gpio_put(LED_PIN_R, 1);
             vTaskDelay(pdMS_TO_TICKS(delay));
             gpio_put(LED_PIN_R, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+        }
+    }
+}
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    int delay = 0;
+
+    while (true) {
+        if (xQueueReceive(yQueueButId, &delay, 0)) {
+            printf("%d\n", delay);
+        }
+
+        if (delay > 0) {
+            gpio_put(LED_PIN_G, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(LED_PIN_G, 0);
             vTaskDelay(pdMS_TO_TICKS(delay));
         }
     }
@@ -61,6 +86,25 @@ void btn_1_task(void *p) {
         }
     }
 }
+void btn_2_task(void *p) {
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+    gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
+
+    int delay = 0;
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_g, pdMS_TO_TICKS(500)) == pdTRUE) {
+            if (delay < 1000) {
+                delay += 100;
+            } else {
+                delay = 100;
+            }
+            printf("delay btn %d \n", delay);
+            xQueueSend(yQueueButId, &delay, 0);
+        }
+    }
+}
 
 int main() {
     stdio_init_all();
@@ -68,9 +112,13 @@ int main() {
 
     xQueueButId = xQueueCreate(32, sizeof(int));
     xSemaphore_r = xSemaphoreCreateBinary();
+    yQueueButId = xQueueCreate(32, sizeof(int));
+    xSemaphore_g = xSemaphoreCreateBinary();
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
